2a24cf9cece7616ca081e198ed7721058174d319
Laurent Malys <lo@crans.org>
2020-11-18 15:26:02 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get install orca tesseract-ocr-all libtesseract4 python3-pyocr python-pyocr python3-tesserocr cython xdotool espeak espeak-ng
+
+pip install numpy
+pip install Scipy
+python -m pip install -U scikit-image
+pip install --upgrade imutils
+
+mkdir -p $HOME/document-scanner
+
+
+chmod +x OCRSCan
